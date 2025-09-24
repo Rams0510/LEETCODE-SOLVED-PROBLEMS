8f4aa130dc7aa6a4b369dfa96b1717667481e113
Thopukonda Ramya <155370533+Rams0510@users.noreply.github.com>
2025-09-24 15:16:57 +0530

    Time: 0 ms (100%), Space: 46 MB (12.83%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,12 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ones = 0, twos = 0;
-
-        for (int num : nums) {
-            ones = (ones ^ num) & ~twos; // update ones
-            twos = (twos ^ num) & ~ones; // update twos
+        int a=0,b=0;
+        for(int n:nums){
+            a^=(n&(~b));
+            b^=(n&(~a));
         }
-
-        return ones; // holds the single number
+        return a;
     }
 }
